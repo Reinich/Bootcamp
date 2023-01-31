2530f791dcd77c706ed9d777fa433facd93ff660
reinich <reinich@outlook.com>
2023-01-31 15:20:51 +0300

    Add lection 1

---
+++ Programming/Lection 1/Task 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+Console.Clear();
+
+int size = 10000;
+int m = 3;
+int[] array = Enumerable.Range(1, size)
+                        .Select(item => Random.Shared.Next(10))
+                        .ToArray();
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+
+Stopwatch sw = new();
+sw.Start();
+
+int max = 0;
+for (int i = 0; i < array.Length - m; i++)
+{
+    int t = 0;
+    for (int j = i; j < i + m; j++)
+    {
+        t += array[j];
+        if (t > max) max = t;
+    }
+}
+
+
+sw.Stop();
+Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+Console.WriteLine(max);
---
+++ Programming/Lection 1/Task 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+Console.Clear();
+
+int size = 10;
+int m = 3;
+int[] array = Enumerable.Range(1, size)
+                        .Select(item => Random.Shared.Next(10))
+                        .ToArray();
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+Stopwatch sw = new();
+sw.Start();
+
+int max = 0;
+for (int j = 0; j < m; j++) max += array[j];
+int t = max;
+for (int i = 1; i < array.Length - m + 1; i++)
+{
+    t = t - array[i - 1] + array[i + (m - 1)];
+    if (t > max) max = t;
+}
+
+sw.Stop();
+Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+Console.WriteLine(max);
